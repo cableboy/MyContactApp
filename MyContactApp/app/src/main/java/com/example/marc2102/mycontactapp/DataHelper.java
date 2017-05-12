525da283c6ecfae07e961ed094b626534ab2fe56
cableboy <cmfootball123@gmail.com>
2017-05-12 10:37:36 -0700

    Added name bar
    yah

--- MyContactApp/app/src/main/java/com/example/marc2102/mycontactapp/DataHelper.java
+++ MyContactApp/app/src/main/java/com/example/marc2102/mycontactapp/DataHelper.java
@@ -10,21 +10,26 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DataHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "Contact.db";
-    public static final String TABLE_NAME ="Contact_table";
+    public static final String TABLE_NAME ="contact_table";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "NAME";
 
-    public DataHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, DATABASE_NAME, null, version);
+    public DataHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
 
     }
 }
--- MyContactApp/app/src/main/java/com/example/marc2102/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/marc2102/mycontactapp/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.marc2102.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    DataHelper myDb;
+    EditText editName;
+    Button btnaddData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         myDb = new DataHelper(this);
     }
 }
