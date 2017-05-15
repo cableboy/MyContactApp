23f98ad6c742232a29e37fbf20d89b1d5ce8c22b
cableboy <cmfootball123@gmail.com>
2017-05-15 10:23:59 -0700

    Adding a contact
    ye

--- MyContactApp/app/src/main/java/com/example/marc2102/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/marc2102/mycontactapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.marc2102.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,6 +18,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         myDb = new DataHelper(this);
+        myDb = new DataHelper(this);
+
+        //add the layout bars
+        editName = (EditText) findViewById(R.id.editText_name);
+    }
+
+
+    public void addData(View v) {
+
+        boolean isInserted = myDb.insertData(editName.getText().toString());
+
+        if (isInserted == true) {
+            Log.d("MyContact", "Success inserting data");
+            //insert Toast message here
+
+        } else {
+            Log.d("MyContact", "Failure inserting data");
+            //insert Toast message here
+
+        }
     }
 }
+
